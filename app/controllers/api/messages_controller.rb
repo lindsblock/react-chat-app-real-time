b7632f0bcb0e874bfb33fadf941ede963c3fbd34
Lindsay Block <lindsay.block05@gmail.com>
2018-04-10 11:21:55 -0600

    initial commit

---
+++ app/controllers/api/messages_controller.rb
@@ -0,0 +1,8 @@
+class Api::MessagesController < ApplicationController
+  before_action :authenticate_user!
+
+  def create
+    MessageBus.publish "/chat_channel", { email: params[:email], body: params[:body]}
+  end
+  
+end
